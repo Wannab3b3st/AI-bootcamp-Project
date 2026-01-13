3f7b6edd9199fd409ce6c4e3f279227b37a9b6e5
hYyym414 <102716725+Wannab3b3st@users.noreply.github.com>
2026-01-13 15:20:25 +0900

    Update index.tsx

--- web/index.tsx
+++ web/index.tsx
@@ -1,14 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 여기서 A가 대문자인지 꼭 확인하세요!
+import './index.css';
 
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <React.StrictMode>
     <App />
