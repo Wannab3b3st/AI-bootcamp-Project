733695afa10777d13d1d3aea8437e50973ce2183
hYyym414 <102716725+Wannab3b3st@users.noreply.github.com>
2026-01-14 11:15:47 +0900

    Update App.jsx

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -4,7 +4,6 @@ import './index.css';
 function App() {
   return (
     <div className="portfolio-wrapper">
-      {/* 장식 없이 텍스트만 깔끔하게 배치 */}
       <h1 className="main-title">
         화훼 경매 금액 예측 AI 서비스<br />
       </h1>
